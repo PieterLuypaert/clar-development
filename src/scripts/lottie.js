694257250119a35c685d8a5c855832b22f9e30b2
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-06-04 19:36:53 +0200

    🎉 Add loadLottieFiles function to initialize Lottie animations on canvas elements

---
+++ src/scripts/lottie.js
@@ -0,0 +1,25 @@
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+
+function loadLottieFiles() {
+    const $canvases = document.querySelectorAll("canvas[data-lottie-source]");
+
+    $canvases.forEach($canvas => {
+        const dotLottie = new DotLottie({
+            autoplay: true,
+            loop: true,
+            canvas: $canvas,
+            speed: 1,
+            layout: {
+                fit: "cover",
+                align: [.5, 0]
+            },
+            src: `/assets/lotties/${$canvas.getAttribute('data-lottie-source')}`
+        });
+
+        window.addEventListener("resize", () => {
+            dotLottie.resize();
+        });
+    });
+}
+
+export default loadLottieFiles;
